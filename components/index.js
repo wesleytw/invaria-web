a0c24aaaae154e13c46e16535394ed0ed696e56c
wesleytw <wesliutw@gmail.com>
2022-07-06 22:12:36 +0800

    D

--- components/index.js
+++
@@ -1,5 +0,0 @@
-// make index for these components
-export {default as Footer} from './Footer';
-export {default as Welcome} from './Welcome';
-export {default as Navbar} from './Navbar';
-export {default as Layout} from './Layout';
